a3a25f76e8113cd2fd309245a0d61cdad194806f
thisisaaronland <aaron@mapzen.com>
2015-07-16 21:24:40 +0000

    placetype is the new place-type

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.utils',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.utils'],
-    version='0.23',
+    version='0.24',
     description='Simple Python wrapper for Who\'s On First helper functions',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-utils',
@@ -30,5 +30,5 @@ setup(
         'scripts/wof-csv-to-feature-collection',
         'scripts/wof-csv-to-s3',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-utils/releases/tag/v0.23',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-utils/releases/tag/v0.24',
     license='BSD')
