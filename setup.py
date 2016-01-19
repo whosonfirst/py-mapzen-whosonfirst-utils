91c5bb69a13607d15f4a2daa5d31bd66aa544f34
thisisaaronland <aaron@mapzen.com>
2016-01-19 22:14:27 +0000

    bump to 0.18

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.utils',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.utils'],
-    version='0.17',
+    version='0.18',
     description='Simple Python wrapper for Who\'s On First helper functions',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-utils',
@@ -42,5 +42,5 @@ setup(
         'scripts/wof-mk-place',
         'scripts/wof-supersede',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-utils/releases/tag/v0.17',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-utils/releases/tag/v0.18',
     license='BSD')
